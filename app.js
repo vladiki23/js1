d832cb71738f390ebdc5223cc43b556e41593146
vladiki23 <73965374+vladiki23@users.noreply.github.com>
2024-04-03 12:30:02 +0200

    Обновление
    Добавлена к предыдущему дз,
    внизу таблицы общее значение area, population.
    без запятых

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-var countries= [];
-var lastIndex = 0;
 fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
     .then(function (countries) {
         console.log(countries);
-        this.countries=countries;
-        clik();
-    });
-    function clik() {
-        const maxElements = 20;
 
         var countriesHtml = "";
-        var counter=0;
+        var totalPopulation = 0;
+        var totalArea = 0;
 
-        for (var index=this.lastIndex; index<Object.keys(this.countries).length;index++) {
-            var element = this.countries[index];
-            const flagUrl = element.flags?.svg;
+        for (var index in countries ) {
+            var country = countries[index];
+            const flagUrl = country.flags?.svg;
+
             countriesHtml += `<tr>
-          <td>${+index + 1}</td>
-          <td>${element.name.common}</td>
-          <td>${element.population}</td>
-          <td>${element.region}</td>
-          <td>${element.area}</td>
-          <td style="background-image: url(${flagUrl}); background-size: 70px 50px; background-position: center; background-repeat: no-repeat;"></td>
-      
-        </tr>`;
-            this.lastIndex=index;
-            counter++;
-            if(maxElements==counter) {
-                this.lastIndex++;
-                counter=0;
-                document.querySelector("#countries-table tbody").innerHTML+=countriesHtml;
-                return;
-            }
+                <td>${+ index +1}</td>
+                <td>${ country.name.common }</td>
+                <td>${ country.population }</td>
+                <td>${ country.region }</td>
+                <td>${ country.area }</td>
+                <td style="background-image: url(${flagUrl}); background-size: 70px 50px; background-position: center; background-repeat: no-repeat;"></td>
+            </tr>`;
+
+            totalPopulation += country.population || 0;
+            totalArea += country.area || 0;
         }
-    }
+
+            countriesHtml += `<tr>
+                <td colspan="2">Total</td>
+                <td>${Math.round(totalPopulation)}</td>
+                <td></td>
+                <td>${Math.round(totalArea)}</td>
+                <td></td>
+            </tr>`;
+
+        document.querySelector('#countries-table tbody').innerHTML = countriesHtml;
+        });
+
